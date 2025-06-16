f20da0dd7c61466af91f41f6f845b371d2cb1469
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-06-16 22:30:44 +0300

    task 01 - Add and done activity

---
+++ 01_node_architecture/models/activity.model.js
@@ -0,0 +1,59 @@
+import { Database } from "../lib/db.js";
+import { getCurrentDate } from "../lib/getCurrentDate.js";
+import { newIdFromArray } from "../lib/newIdFromArray.js";
+
+const db = new Database();
+
+export function addNew(name, freq) {
+  const allItems = db.get();
+  const createdAt = getCurrentDate();
+  const id = newIdFromArray(allItems);
+
+  const item = {
+    id,
+    name,
+    freq,
+    createdAt,
+    updatedAt: createdAt,
+    doneLog: [],
+  };
+
+  allItems.push(item);
+  db.save(allItems);
+  return item;
+}
+
+export function getAll() {
+  return db.get();
+}
+
+export function setDoneById(id) {
+  const allItems = db.get();
+  const date = getCurrentDate();
+
+  const item = allItems.find(i => i.id === id);
+  if (!item) return null;
+
+  if (!Array.isArray(item.doneLog)) {
+    item.doneLog = [];
+  }
+
+  if (!item.doneLog.includes(date)) {
+    item.doneLog.push(date);
+  }
+
+  db.save(allItems);
+  return item;
+}
+
+export function getStats() {
+  return "stats";
+}
+
+export function removeById(id) {
+  return "deleted";
+}
+
+export function updateById(id, name, freq) {
+  return "updated";
+}

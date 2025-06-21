677ed13b2f8dc52b6965f4f9d671a1629571fc60
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-06-21 18:03:16 +0300

    task 2 - default route

--- 02_server_basics/lib/router.js
+++ 02_server_basics/lib/router.js
@@ -1,3 +1,16 @@
-export function router(req, res) {
-    res.end('I am here from router');
+import { dirname, join } from 'path';
+import { fileURLToPath } from "url";
+
+export async function router(req, res) {
+    const currentDir = dirname(fileURLToPath(import.meta.url));
+    const routesDir = join(currentDir, '..', 'routes');
+
+    const routePath = join(routesDir, 'route.js');
+    const routeHandler = await import(routePath);
+
+    try {
+        routeHandler.default(req, res);
+    } catch (e) {
+        console.log('error', e);
+    }
 }

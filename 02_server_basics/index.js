919425aba478848f335557ab3e85082f5cde4039
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-06-21 16:30:44 +0300

    task 02 - start

--- 02_server_basics/index.js
+++ 02_server_basics/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import 'dotenv/config';
+import * as http from "node:http";
+
+const server = http.createServer((req, res) => {
+    res.end('Hello world!!!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server started at port: ${PORT}`);
+});

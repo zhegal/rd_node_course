416ac313f7bdcc2db9b932d0017cd3d6de1c0f04
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-07-03 22:45:56 +0300

    task 4 - error handler and env example

--- 04_node_express/src/middlewares/errorHandler.js
+++ 04_node_express/src/middlewares/errorHandler.js
@@ -1,6 +1,5 @@
-export function errorHandler(err, req, res) {
+// eslint-disable-next-line no-unused-vars
+export function errorHandler(err, _req, res, _next) {
     console.error(err);
-    req.log.error({ err });
-
     res.status(err.status || 500).json({ error: err.message || 'Server error' });
 }

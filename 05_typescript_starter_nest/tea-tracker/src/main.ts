eb720fd3dbf97aff7e2f12e2bdc66c5c14135579
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-07-05 23:28:53 +0300

    task 5 - swagger added

--- 05_typescript_starter_nest/tea-tracker/src/main.ts
+++ 05_typescript_starter_nest/tea-tracker/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Demo API')
+    .setDescription('Mini-проєкт Nest')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();

8ae89401f21c34c21309e617975c18b177483dd0
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-07-06 00:03:43 +0300

    task 5 - dotenv

--- 05_typescript_starter_nest/tea-tracker/src/app.module.ts
+++ 05_typescript_starter_nest/tea-tracker/src/app.module.ts
@@ -2,9 +2,10 @@ import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeaModule } from './tea/tea.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TeaModule],
+  imports: [ConfigModule.forRoot(), TeaModule],
   controllers: [AppController],
   providers: [AppService, Logger],
 })

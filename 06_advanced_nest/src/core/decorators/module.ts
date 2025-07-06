6f1d97cc505c31ca646747911ad0c034faa2dd55
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-07-07 02:13:27 +0300

    task 6 - modules start

---
+++ 06_advanced_nest/src/core/decorators/module.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+
+interface ModuleMetadata {
+  providers?: any[];
+  controllers?: any[];
+  imports?: any[];
+}
+
+export function Module(metadata: ModuleMetadata) {
+  return function (target: any) {
+    Reflect.defineMetadata("module:metadata", metadata, target);
+  };
+}

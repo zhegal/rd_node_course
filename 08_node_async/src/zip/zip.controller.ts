dcce7b40cb6adeebf0921c01cb29fd617d65dcd9
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-07-13 20:36:29 +0300

    task 8 - saving zip file to temp

--- 08_node_async/src/zip/zip.controller.ts
+++ 08_node_async/src/zip/zip.controller.ts
@@ -6,13 +6,37 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ZipService } from './zip.service';
+import { diskStorage } from 'multer';
+import { tmpdir } from 'os';
 
 @Controller('zip')
 export class ZipController {
   constructor(private readonly zipService: ZipService) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: tmpdir(),
+        filename: (_, file, cb) => {
+          cb(null, `${Date.now()}-${file.originalname}`);
+        },
+      }),
+      fileFilter: (_, file, cb) => {
+        if (
+          file.mimetype === 'application/zip' ||
+          file.originalname.endsWith('.zip')
+        ) {
+          cb(null, true);
+        } else {
+          cb(new Error('Only .zip files supports'), false);
+        }
+      },
+      limits: {
+        fileSize: 100 * 1024 * 1024,
+      },
+    }),
+  )
   async sendArchive(@UploadedFile() file: Express.Multer.File) {
     return this.zipService.sendArchive(file);
   }
--- 08_node_async/src/zip/zip.service.ts
+++ 08_node_async/src/zip/zip.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ZipService {
   async sendArchive(file: Express.Multer.File) {
-    console.log(file);
     return {
-      message: file ? 'i have a file!!!' : 'send me file please!!',
+      message: 'ZIP file saved to tmp directory',
+      originalName: file.originalname,
+      savedPath: file.path,
+      size: file.size,
     };
   }
 }

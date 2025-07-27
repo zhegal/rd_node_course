e1021eb9a106266d54c71c315c76ab951cd87144
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-07-27 19:04:34 +0300

    task 9 - connection and disconnection

--- 09_reactivity/server/src/ws/chat.gateway.ts
+++ 09_reactivity/server/src/ws/chat.gateway.ts
@@ -18,6 +18,8 @@ const INSTANCE_ID = uuid(); // 🎯 унікальний для кожної р�
 export class ChatGateway implements OnGatewayConnection, OnModuleDestroy {
   private readonly sub: Redis;
   private event$ = new Subject<{ ev: string; data: any; meta?: any }>();
+  private clients = new Map<string, Socket>();
+  private chatMembers = new Map<string, Set<string>>();
 
   constructor(private store: Store, private readonly redis: Redis) {
     this.sub = this.redis.duplicate();
@@ -28,6 +30,15 @@ export class ChatGateway implements OnGatewayConnection, OnModuleDestroy {
       if (parsed.src === INSTANCE_ID) return; // ⬅️ skip own
       console.log("Received event:", parsed);
       this.event$.next(parsed);
+    });
+
+    this.event$.pipe(filter((e) => e.ev === "join")).subscribe(({ data }) => {
+      const { chatId, user } = data;
+      if (!this.chatMembers.get(chatId)) {
+        this.chatMembers.set(chatId, new Set());
+      }
+      const members = this.chatMembers.get(chatId);
+      members?.add(user);
     });
 
     this.event$
@@ -50,8 +61,14 @@ export class ChatGateway implements OnGatewayConnection, OnModuleDestroy {
 
     if (!user) return client.disconnect(true);
     client.data.user = user;
+    this.clients.set(user, client);
 
     // forward broadcast events belonging to this user
+  }
+
+  handleDisconnect(client: Socket) {
+    const user = client.handshake.auth?.user as string;
+    this.clients.delete(user);
   }
 
   @SubscribeMessage("join")
@@ -59,7 +76,15 @@ export class ChatGateway implements OnGatewayConnection, OnModuleDestroy {
     @ConnectedSocket() client: Socket,
     @MessageBody() body: { chatId: string }
   ) {
-    console.log("join");
+    const user = client.handshake.auth?.user as string;
+    const data = { ...body, user };
+    this.redis.publish(
+      "chat-events",
+      JSON.stringify({
+        ev: "join",
+        data,
+      })
+    );
   }
 
   @SubscribeMessage("leave")

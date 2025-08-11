8c789452567bd13ddefe804e40a6e0105311da2c
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-08-11 11:22:19 +0300

    task 4 - logging in error handler

--- 04_node_express/src/middlewares/errorHandler.js
+++ 04_node_express/src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
-export function errorHandler(err, req, res, _next) {
-    console.error(err)
+export function errorHandler(err, req, res) {
+    console.error(err);
     req.log.error({ err });
     res.status(err.status || 500).json({ error: err.message || 'Server error' });
 }
